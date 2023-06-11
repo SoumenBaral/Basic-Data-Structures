2f20a69393ad5f83634b8fe5707284e5f27c395f
SoumenBaral <shumen.sb@gmail.com>
2023-06-11 21:01:50 +0600

    Link list done

---
+++ 2.Link list/1.Singly linked  list/print_link.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node*next;
+        
+        Node(int val)
+        {
+            this->val=val;
+            this->next=NULL;
+        }
+};
+int main ()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+    
+    head->next=a;
+    a->next=b;
+    b->next=c;
+    c->next=d;
+    // cout<<head->val<<endl;
+    // cout<<head->next->val<<endl;
+    // cout<<head->next->next->val<<endl;
+    // cout<<head->next->next->next->val<<endl;
+
+    // while(head !=NULL)
+    // {
+    //     cout<<head->val<<endl; //if we do it we will lost our head so you have a take a temporay variable .
+    //     head=head->next;
+    // }
+    Node* tmp =head;
+    while(tmp != NULL)
+    {
+        cout<<tmp->val<<endl;
+        tmp=tmp->next;
+    }
+    
+
+    return 0;
+}
