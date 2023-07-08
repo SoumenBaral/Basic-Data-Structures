8841f719804c45bea811f13a75ce5d0dc2c2039f
SoumenBaral <shumen.sb@gmail.com>
2023-07-08 22:41:29 +0600

    Binary tree and preOrder and postOrder

---
+++ 5.Binary Tree/1. Binary Tree Implementation/3.PostOrder.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+        
+        Node(int val){
+            this->val = val;
+            this->left = NULL;
+            this->right= NULL;
+        }
+};
+void postOrder(Node* root){
+
+    // Base case 
+    if(root == NULL)return;
+
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->val<<" ";
+
+}
+int main ()
+{
+    Node* root = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+    Node* e = new Node(60);
+    Node* f = new Node(70);
+    Node* g = new Node(80);
+    Node* h = new Node(90);
+    Node* i = new Node(100);
+
+    //connection;
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = h;
+    b->right = d;
+    c->left = e;
+    h->left = i;
+    d->left = f;
+    d->right = g;
+
+    postOrder(root);
+    return 0;
+}
