8fefdb70e9551e3698faed4a2265afd9ab3eff13
SoumenBaral <shumen.sb@gmail.com>
2023-07-10 00:12:01 +0600

    input and level Order binary tree

--- 5.Binary Tree/2. Operation On Binary Tree/1.LevelOrderPrint.cpp
+++ 5.Binary Tree/2. Operation On Binary Tree/1.LevelOrderPrint.cpp
@@ -35,6 +35,49 @@ void level_Order(Node* root){
     }
     
 };
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ano
+        Node *p = q.front();
+        q.pop();
+
+        // 2. jabotiyo ja kaj ache
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft;
+        Node *myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        // 3. children gulo ke push koro
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
 int main ()
 {
     Node* root = new Node(10);
@@ -60,6 +103,6 @@ int main ()
     d->right = g;
 
     level_Order(root);
-    
+
     return 0;
 }
