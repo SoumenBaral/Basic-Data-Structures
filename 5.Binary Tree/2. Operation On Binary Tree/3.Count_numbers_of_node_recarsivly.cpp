f54111923d2d016466dfc60a8ef321764d440d21
SoumenBaral <shumen.sb@gmail.com>
2023-07-10 17:26:41 +0600

    brinary tree count tree count leaf count max height done

---
+++ 5.Binary Tree/2. Operation On Binary Tree/3.Count_numbers_of_node_recarsivly.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+        
+        Node(int val){
+            this->val = val;
+            this->left = NULL;
+            this->right= NULL;
+        }
+};
+Node* input_tree()
+{
+    int val;
+    cin>>val;
+    Node* root;
+    if(val==-1)root = NULL;
+    else root = new Node(val);
+    queue<Node*>q;
+    if(root)q.push(root);
+    while (!q.empty())
+    {
+        //1. queue dia front ta bar kora
+        Node* f = q.front();
+        q.pop();
+
+        // 2. kaj gula kora 
+        int l,r;
+        cin>>l>>r;
+        Node* left ;
+        Node* right;
+        if( l == -1)left=NULL;
+        else left = new Node(l);
+        if(r == -1)right = NULL;
+        else right = new Node (r);
+        f->left = left;
+        f->right = right;
+
+          // 3. children bar kore nia asa 
+       if(f->left)q.push(f->left);
+       if(f->right)q.push(f->right);
+        
+    }
+    return root;
+     
+}
+
+int count(Node* root){
+    if(root==NULL) return 0;
+    int l = count(root->left);
+    int r = count(root->right);
+    return l+r+1;
+
+}
+int main ()
+{
+    Node*root = input_tree();
+    cout<<count(root)<<endl;
+    return 0;
+}
