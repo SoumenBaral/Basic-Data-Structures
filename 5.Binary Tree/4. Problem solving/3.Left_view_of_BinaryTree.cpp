03c8275aa0d0fb29e103f259555677510ce7a64c
SoumenBaral <shumen.sb@gmail.com>
2023-07-12 12:14:15 +0600

    Left view of the Binary tree

---
+++ 5.Binary Tree/4. Problem solving/3.Left_view_of_BinaryTree.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <bits/stdc++.h> 
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+
+
+vector<int> getLeftView(TreeNode<int> *root)
+{
+    //    Write your code here
+    bool frq[3010] = {false};
+    queue<pair<TreeNode<int>*,int>>q;
+    if(root)q.push({root,1});
+    vector<int>v;
+    while(!q.empty()){
+        pair<TreeNode<int> *,int> parent = q.front();
+        q.pop();
+        TreeNode<int> *node = parent.first;
+        int level = parent.second;
+        if(frq[level]==false){
+            v.push_back(node->data);
+            frq[level]=true;
+        }
+
+        if(node->left)q.push({node->left,level+1});
+        if(node->right)q.push({node->right,level+1});
+
+        
+    }
+
+return v;
+}
+int main ()
+{
+    
+    return 0;
+}
