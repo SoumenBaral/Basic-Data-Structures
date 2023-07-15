4c671ef9100769f62918ba9e00ecb40293e0b1ce
SoumenBaral <shumen.sb@gmail.com>
2023-07-16 01:15:16 +0600

    Search In BST

---
+++ 6. Bst Heap priority Queue and Map/tempCodeRunnerFile.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+        
+        Node(int val){
+            this->val = val;
+            this->left = NULL;
+            this->right= NULL;
+        }
+};
+Node* input_tree()
+{
+    int val;
+    cin>>val;
+    Node* root;
+    if(val==-1)root = NULL;
+    else root = new Node(val);
+    queue<Node*>q;
+    if(root)q.push(root);
+    while (!q.empty())
+    {
+        //1. queue dia front ta bar kora
+        Node* f = q.front();
+        q.pop();
+
+        // 2. kaj gula kora 
+        int l,r;
+        cin>>l>>r;
+        Node* left ;
+        Node* right;
+        if( l == -1)left=NULL;
+        else left = new Node(l);
+        if(r == -1)right = NULL;
+        else right = new Node (r);
+        f->left = left;
+        f->right = right;
+
+          // 3. children bar kore nia asa 
+       if(f->left)q.push(f->left);
+       if(f->right)q.push(f->right);
+        
+    }
+    return root;
+     
+}
+// void print_tree(Node* root){
+//     if(root==NULL)return;
+//     cout<<root->val<<" ";
+
+//     if(root->left)print_tree(root->left);
+//     if(root->right)print_tree(root->right);
+// }
+void level_Order_print(Node* root){
+    queue<Node*>q;
+    q.push(root);
+    while (!q.empty())
+    {
+        //Bar koira ana..
+        Node* f = q.front();
+        q.pop();
+
+        //ja kaj korar korboo
+
+        cout<<f->val<<" ";
+
+        //children gola bar koira ana 
+
+        if (f->left)q.push(f->left);
+        if(f->right)q.push(f->right);
+
+        
+    }
+    
+};
+bool search(Node* root,int val){
+    if(root==NULL)return false;
+    if(root->val==val)return true;
+    if(val<root->val){
+        bool l = search(root->left,val);
+        if(l) return true;
+        else return false;
+    }
+    if(val>root->val){
+        bool r =search(root->right,val);
+        if(r)return true ;
+        else return false; 
+
+    }
+}
+int main ()
+{
+    Node* root = input_tree();
+    // print_tree(root);
+    level_Order_print(root);
+ search( root,3);
+
+    return 0;
+}
